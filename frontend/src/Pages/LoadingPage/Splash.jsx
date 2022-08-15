0afb8c758883735e455b79685e48bb0090fe0d18
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2022-08-15 22:42:39 +0500

    .

---
+++ frontend/src/Pages/LoadingPage/Splash.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./splash.css";
+import logo from "./assets/logo.png";
+
+const Splash = () => {
+  return (
+    <div>
+      <img src={logo} alt="#" />
+      <p>YOUR EXPERIENCE IS BEING OPTIMISED</p>
+    </div>
+  );
+};
+
+export default Splash;
